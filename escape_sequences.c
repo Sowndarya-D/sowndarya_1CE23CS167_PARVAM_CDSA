c2c39ceeeec064060c88d7a0a4057e4c30e5b43d
Sowndarya-D <sowndaryad59@gmail.com>
2024-11-05 10:08:45 +0530

    Add files via upload

---
+++ escape_sequences.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("demonstrating escape sequences:\\ \n");
+    //newline
+    printf("the sequence of the \\\\ first line\\\\\\ \\ ""the sequence of\\\\"" second line\n");
+    printf("the cost of newspaper\t is:$\n");
+    printf("enter the value of a ");
+    return 0;
+}
