025cacb85cd8dc6d751183e2d36ddab66a01e9e7
Sowndarya-D <sowndaryad59@gmail.com>
2024-11-05 10:58:50 +0530

    Add files via upload

---
+++ prog_7.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    //%f(float),%s(string),%c(char)
+    float b=10.0;
+    char c='a';
+    char str[]="hello";
+    printf("%f\n",b);
+    printf("%c\n",c);
+    printf("%s\n",str);
+       return 0;
+}
