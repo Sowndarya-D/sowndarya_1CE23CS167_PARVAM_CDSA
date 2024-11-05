51f599aae59350ad310b55f2e3c1dfe2a772801c
Sowndarya-D <sowndaryad59@gmail.com>
2024-11-05 10:58:27 +0530

    Add files via upload

---
+++ outputformatting.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a=9;
+//    ( %d(decimal),%o(octal),%x(hexadecimal lwr_case),%X(hexadecimal upr_case));
+    printf(" %d\n ",a);
+    printf(" %o \n",a);
+    printf(" %x \n",a);
+    printf(" %X \n",a);
+    return 0;
+}
