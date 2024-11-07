3cd6f2df526d2106a9685c5a0a22cdd3716127ca
Sowndarya-D <sowndaryad59@gmail.com>
2024-11-07 11:30:18 +0530

    Add files via upload

---
+++ recurssion_fun.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int num=7;
+    int res=factorial(num);
+    printf("%d",res);
+    return 0;
+}
+int factorial(int n){
+    if(n==0){
+        return 1;
+    }
+    else{
+        return n*factorial(n-1);
+
+    }
+}
